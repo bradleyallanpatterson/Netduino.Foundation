f3c8ef7153114b2e76e42a4b946624d77382adb1
Adrian Stevens <adrian.stevens@gmail.com>
2018-11-01 18:43:51 -0700

    Added Meadow logo drawing code to ePaper sample

--- Source/Peripheral_Libs/Displays.Waveshare-ePaper/Samples/WaveShareSample/Program.cs
+++ Source/Peripheral_Libs/Displays.Waveshare-ePaper/Samples/WaveShareSample/Program.cs
@@ -16,39 +16,99 @@ namespace WaveShareEPaper
         static PushButton button;
         static Led led;
 
+        private static void DrawLogo ()
+        {
+            var bottom = 265;
+            var mHeight = 54;
+
+            //mountain fill
+            int lineWidth, x, y;
+
+            for (int i = 1; i < mHeight - 1; i++)
+            {
+                y = bottom - i;
+                x = 4 + 1 + i * 20 / 27;
+
+                //fill bottom of mountain
+                if (i < mHeight / 2)
+                {
+                    lineWidth = 38;
+                    display.DrawLine(x, y, x + lineWidth, y, Color.Red);
+                }
+                //fill top of mountain
+                else
+                {
+                    lineWidth = 38 - (i - mHeight / 2) * 40 / 27;
+                    display.DrawLine(x, y, x + lineWidth, y, Color.Red);
+                }
+            }
+
+            //mountains
+            display.DrawLine(4, bottom, 44, bottom - mHeight, true);
+            display.DrawLine(4, bottom, 44, bottom, true);
+            display.DrawLine(44, bottom - mHeight, 64, bottom - mHeight / 2, true);
+
+            display.DrawLine(44, bottom, 84, bottom - mHeight, true);
+            display.DrawLine(84, bottom - mHeight, 124, bottom, true);
+
+            // trees
+            int heightTree = 7;
+            int yStart, yEnd;
+            for (int i = 0; i < 3; i++)
+            {
+                yStart = bottom - i * heightTree;
+                yEnd = bottom - (i + 1) * heightTree;
+
+                display.DrawLine(61, yStart, 68, yEnd, Color.Red);
+                display.DrawLine(68, yEnd, 75, yStart, Color.Red);
+
+                display.DrawLine(77, yStart, 84, yEnd, Color.Red);
+                display.DrawLine(84, yEnd, 91, yStart, Color.Red);
+
+                display.DrawLine(93, yStart, 100, yEnd, Color.Red);
+                display.DrawLine(100, yEnd, 107, yStart, Color.Red);
+            }
+
+       //     display.Show();
+        }
+
         public static void Main()
         {
-            ePaper = new EPD2i13b(chipSelectPin: Pins.GPIO_PIN_D4,
+            ePaper = new EPD2i9b(chipSelectPin: Pins.GPIO_PIN_D4,
                 dcPin: Pins.GPIO_PIN_D7,
                 resetPin: Pins.GPIO_PIN_D6,
                 busyPin: Pins.GPIO_PIN_D5,
                 spiModule: SPI.SPI_module.SPI1,
                 speedKHz: 4000);
             
-       //     ePaper.Clear(false, true);
+            ePaper.Clear(false, true);
 
             display = new GraphicsLibrary(ePaper);
 
-            display.DrawLine(10, 10, 94, 202);
+            DrawLogo();
+
+            display.DrawLine(10, 50, 118, 170);
          //   display.Show();
          //   Thread.Sleep(500);
 
-            display.DrawCircle(52, 60, 32);
+            display.DrawCircle(52, 80, 32);
           //  display.Show();
          //   Thread.Sleep(500);
 
-            display.DrawRectangle(5, 40, 60, 60);
+            display.DrawRectangle(5, 60, 60, 60);
           //  display.Show();
           //  Thread.Sleep(1000);
 
-            display.DrawRectangle(20, 100, 40, 25, Color.Red, true);
+            display.DrawRectangle(20, 115, 40, 25, Color.Red, true);
          //   display.Show();
         //    Thread.Sleep(500);
 
             display.CurrentFont = new Font8x12();
-            display.DrawText(2, 2, "Wilderness");
-            display.CurrentFont = new Font8x8();
-            display.DrawText(2, 16, "Netduino 3", Color.Red);
+            display.DrawText(4, 4, "Wilderness Labs", Color.Red);
+          //  display.CurrentFont = new Font8x8();
+            display.DrawText(40, 20, "Meadow");
+
+            DrawLogo();
 
             display.Show();
 
